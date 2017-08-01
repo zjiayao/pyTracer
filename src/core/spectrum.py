eee2da8fb1256fde40c920582aa43f9d8d464ae9
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-08-01 17:26:02 +0800

    change returning `List` to `np.ndarray`

--- src/core/spectrum.py
+++ src/core/spectrum.py
@@ -17,15 +17,15 @@ N_SPECTRAL_SAMPLES = 30
 
 @jit
 def xyz2rgb(xyz: [FLOAT]) -> [FLOAT]:
-	return [ 3.240479 * xyz[0] - 1.537150 * xyz[1] - 0.498535 * xyz[2],
-			-0.969256 * xyz[0] + 1.875991 * xyz[1] + 0.041556 * xyz[2],
-			 0.055648 * xyz[0] - 0.204043 * xyz[1] + 1.057311 * xyz[2]]
+	return np.array([ 3.240479 * xyz[0] - 1.537150 * xyz[1] - 0.498535 * xyz[2],
+					-0.969256 * xyz[0] + 1.875991 * xyz[1] + 0.041556 * xyz[2],
+					 0.055648 * xyz[0] - 0.204043 * xyz[1] + 1.057311 * xyz[2]])
 
 @jit
 def rgb2xyz(rgb: [FLOAT]) -> [FLOAT]:
-	return[ 0.412453 * rgb[0] + 0.357580 * rgb[1] + 0.180423 * rgb[2],
-			0.212671 * rgb[0] + 0.715160 * rgb[1] + 0.072169 * rgb[2],
-			0.019334 * rgb[0] + 0.119193 * rgb[1] + 0.950227 * rgb[2]]
+	return np.array([0.412453 * rgb[0] + 0.357580 * rgb[1] + 0.180423 * rgb[2],
+					0.212671 * rgb[0] + 0.715160 * rgb[1] + 0.072169 * rgb[2],
+					0.019334 * rgb[0] + 0.119193 * rgb[1] + 0.950227 * rgb[2]])
 
 # Spectral Data
 from src.spectral.data import *
