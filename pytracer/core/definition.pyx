8497c1451a4f92302e8dfb411429830db3c4564b
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-08-29 19:55:09 +0800

    add C header for constants

--- pytracer/core/definition.pyx
+++
@@ -1,5 +0,0 @@
-import numpy as np
-FLOAT = np.float32
-INT = np.int32
-UINT32 = np.uint32
-UINT8 = np.uint8
---
+++ pytracer/include/constant.hpp
@@ -0,0 +1,14 @@
+/*
+ * Constant.hpp
+ *
+ * Defines constant for interfacing Cython
+ *
+ * Created by Jiayao on Aug 29, 2017
+ */
+#include <cmath>
+
+double EPS = 1e-5,
+       PI =  3.14159265358979323846,
+       INV_PI = 0.31830988618379067154,
+       INV_2PI = 0.15915494309189533577,
+       INF = INFINITY;
